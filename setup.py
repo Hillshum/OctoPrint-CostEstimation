6687d780cc922f53f568e032a894748ae1426b54
Hilton Shumway <hillshum@gmail.com>
2024-11-21 19:17:28 -0600

    Really fix version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 plugin_identifier = "costestimation"
 plugin_package = "octoprint_costestimation"
 plugin_name = "OctoPrint-CostEstimation"
-plugin_version = "3.5.1-nightly-2024-11-21"
+plugin_version = "3.5.2-nightly-2024-11-21"
 plugin_description = "Displays the estimated print cost for the loaded model"
 plugin_author = "Sven Lohrmann, Olli, Hilton Shumway"
 plugin_author_email = "hillshum@gmail.com, ollisgit@gmail.com, malnvenshorn@mailbox.org"
